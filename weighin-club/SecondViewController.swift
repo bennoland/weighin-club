a4404aa524e5684edd5490dd89803b96ba8c0c2f
Ben Noland <ben.noland@gmail.com>
2018-12-06 16:45:16 -0700

    initial commit

--- weighin-club/SecondViewController.swift
+++ weighin-club/SecondViewController.swift
@@ -1,18 +1,9 @@
-//
-//  SecondViewController.swift
-//  weighin-club
-//
-//  Created by Ben Noland on 12/6/18.
-//  Copyright © 2018 Ben Noland. All rights reserved.
-//
-
 import UIKit
 
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
