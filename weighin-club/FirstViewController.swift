6566867a2df1f73dda0c5f4bf66ea059973b6f85
Ben Noland <ben.noland@gmail.com>
2018-12-06 16:43:04 -0700

    Initial Commit

---
+++ weighin-club/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  weighin-club
+//
+//  Created by Ben Noland on 12/6/18.
+//  Copyright © 2018 Ben Noland. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
